251d8348d3bd701104dca31fb6dd82419a8c846a
javicuriel <javiercuriel94@gmail.com>
2016-08-22 13:50:45 -0500

    Resultado mas exacto

--- Tarea_2/raizNewton.hs
+++ Tarea_2/raizNewton.hs
@@ -1,2 +1,9 @@
-raiz x = raizI (1,0,x)
-raizI (x,i,n) = if i > 20 then x else raizI ((x+n/x)/2,i+1,n)
+
+-- Exactitud con un limite de iteraciones
+-- raiz x = raizI (1,0,x)
+-- raizI (x,i,n) = if i > 20 then x else raizI ((x+n/x)/2,i+1,n)
+
+
+-- Exactitud con un limite de tamaño de resultado (mas exacto)
+raiz x = raizI (1,(1+x/1)/2,x)
+raizI (x,i,n) = if x==i then x else raizI ((x+n/x)/2,(i+n/i)/2,n)
