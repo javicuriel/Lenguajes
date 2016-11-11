864222c4b06d224b78cbd1c513dcbbabda300bdf
javicuriel <javiercuriel94@gmail.com>
2016-11-11 09:23:05 -0600

    Right Rotation

--- arboles/avl.pl
+++ arboles/avl.pl
@@ -1,9 +1,13 @@
 height(empty,0).
+
 height(node(_,Left,Right),Sol):-height(Left,SL),height(Right,SR),Sol is max(SL,SR)+1.
 
 factor(node(_,Left,Right),Sol):-height(Left,L),height(Right,R),Sol is L-R.
 
 leftRotation(node(APadre,PadreLeft,node(APivote,PivotLeft,PivoteRight)),node(APivote,node(APadre,PadreLeft,PivotLeft),PivoteRight)).
+
+rightRotation(node(APadre,node(APivote,PivotLeft,PivoteRight),PadreRight),node(APivote,PivotLeft,node(APadre,PadreRight,PivoteRight))).
+
 
 % leftRotation(node(7,node(6,empty,empty),node(9,node(8,empty,empty),node(10,empty,node(11,empty,empty)))),N).
 %
