580a64251da8ad5cb64e0d213ee84aad241302e1
javicuriel <javiercuriel94@gmail.com>
2016-11-25 10:50:38 -0600

    Added node view

--- ExamenFinal/binaryHeap.pl
+++ ExamenFinal/binaryHeap.pl
@@ -8,12 +8,12 @@ getPos(A,L,N):-getPosH(A,1,L,N).
 getLoc(N,N,[H|L],H).
 getLoc(N,I,[H|L],A):-I2 is I+1,getLoc(N,I2,L,A).
 
-insertAt(empty,I,N,List,List).
+insertAt(empty,I,N,List,List):-!.
 insertAt(A,N,N,[H|T],[A|List]):-insertAt(empty,I,N,T,List).
 insertAt(A,I,N,[H|L],[H|List]):-I2 is I+1,insertAt(A,I2,N,L,List).
 
 insert(X,[ ],[X]).
-insert(X,[H|T],[H|Z]) :- insert(X,T,Z).
+insert(X,[H|T],[H|Z]):-insert(X,T,Z).
 
 check(X,K,LastK,List,NewList):-K=:=1,getLoc(K,1,List,Switch),insertAt(Switch,1,LastK,List,TempList),insertAt(X,1,K,TempList,NewList).
 check(X,K,LastK,List,NewList):-K2 is floor(K/2),getLoc(K2,1,List,Parent),Parent<X,getLoc(K,1,List,Switch),insertAt(Switch,1,LastK,List,TempList),check(X,K2,K,TempList,NewList).
@@ -23,9 +23,17 @@ insertList([],List,List).
 insertList([H|L],List,N):-insert(H,List,NewList),getPos(H,NewList,Pos),check(H,Pos,Pos,NewList,NewTList),insertList(L,NewTList,N).
 
 
-nodeInsert(N, empty, node(N,empty,empty)).
-nodeInsert(N, node(E,Left,Right), node(E, Left, NewRight)):- N>E, nodeInsert(N, Right, NewRight).
-nodeInsert(N, node(E,Left,Right), node(E,NewLeft,Right)):-N<E, nodeInsert(N, Left, NewLeft).
+% insertN(N, empty, node(N,empty,empty)).
+% insertN(N, node(E,Left,Right), node(E, Left, NewRight)):- N>E, insertN(N, Right, NewRight).
+% insertN(N, node(E,Left,Right), node(E,NewLeft,Right)):-N<E, insertN(N, Left, NewLeft).
 
-listNode([],Tree,Tree).
-listNode([H|T],Tree,N):-listNode(H,Tree,NewTree),listNode(T,NewTree,N).
+height(empty,0).
+height(node(_,Left,Right),Sol):-height(Left,SL),height(Right,SR),Sol is max(SL,SR)+1.
+
+insertN(N, empty, node(N,empty,empty)).
+insertN(N,node(A,L,R),node(A,L,NewR)):-height(L,HL),height(R,HR),HL>HR,insertN(N,R,NewR).
+insertN(N,node(A,L,R),node(A,NewL,R)):-insertN(N,L,NewL).
+
+
+insertListN([],Tree,Tree).
+insertListN([H|T],Tree,N):-insertN(H,Tree,NewTree),insertListN(T,NewTree,N).
